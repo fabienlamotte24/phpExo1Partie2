fe4713bf667831f94a79bde3c75717319b1b0db0
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 08:59:08 +0200

    exo1 p2 php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <p>
+<?php
+  $age = 26;
+  if($age >= 18){
+    echo 'vous êtes majeur';
+  } else {
+    echo 'vous êtes mineur';
+  }
+ ?>
+    </p>
+  </body>
+</html>
