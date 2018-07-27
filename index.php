7b7932310fddbd991bc8d4921344cf00bc3962f6
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-27 14:45:38 +0200

    correction avec la classe

--- index.php
+++ index.php
@@ -8,12 +8,17 @@
     <p>
 <?php
 /*Définition de la variable*/
-  $age = 26;
-  /*Condition si variable est sup à 18*/
-  if($age >= 18){
-    echo 'vous êtes majeur';
-  } else {/*Autre condition si la variable n'est pas sup à 18*/
-    echo 'vous êtes mineur';
+  $age = -3;
+  /*Condition pour éviter les age négatifs*/
+  if($age < 0){
+    echo 'Veuillez entrer un age valide';
+  } else {
+    /*Condition si variable est sup à 18*/
+    if($age >= 18){
+      echo 'vous êtes majeur';
+    } else {/*Autre condition si la variable n'est pas sup à 18*/
+      echo 'vous êtes mineur';
+    }
   }
  ?>
     </p>
